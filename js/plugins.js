37deb9e9c5ae24c996cb8a2aae665591ebf01d27
Felix Lau <felixlaumon@gmail.com>
2011-04-03 01:03:47 -0400

    uses cass; added dygraphs

--- js/plugins.js
+++ js/plugins.js
@@ -1,15 +1,34 @@
 
-// usage: log('inside coolFunc', this, arguments);
-// paulirish.com/2009/log-a-lightweight-wrapper-for-consolelog/
+(function($){
+
+ 
+
+
+
+
+
+ 
+
+
+
+})(this.jQuery);
+
+
+
+
 window.log = function(){
-  log.history = log.history || [];   // store logs to an array for reference
+  log.history = log.history || [];   
   log.history.push(arguments);
-  arguments.callee = arguments.callee.caller;  
-  if(this.console) console.log( Array.prototype.slice.call(arguments) );
+  if(this.console){
+    console.log( Array.prototype.slice.call(arguments) );
+  }
 };
-// make it safe to use console.log always
-(function(b){function c(){}for(var d="assert,count,debug,dir,dirxml,error,exception,group,groupCollapsed,groupEnd,info,log,markTimeline,profile,profileEnd,time,timeEnd,trace,warn".split(","),a;a=d.pop();)b[a]=b[a]||c})(window.console=window.console||{});
+(function(doc){
+  var write = doc.write;
+  doc.write = function(q){ 
+    log('document.write(): ',arguments); 
+    if (/docwriteregexwhitelist/.test(q)) write.apply(doc,arguments);  
+  };
+})(document);
 
-
-// place any jQuery/helper plugins in here, instead of separate, slower script files.
 
--- js/script.js
+++ js/script.js
@@ -3,7 +3,40 @@
 */
 
 
+g = new Dygraph(
+	document.getElementById("graphdiv"),  // containing div
+	"1,12\n" +
+	"2,28\n" +
+	"3,32\n" +
+	"4,41\n" +
+	"5,23\n" +
+	"6,27\n" +
+	"7,17\n" +
+	"8,27\n" +
+	"9,77\n" +
+	"10,23\n" +
+	"11,24\n" +
+	"12,58\n",
+	{
+		width: 700,
+		height: 70,
+		labels: [ "Date", "Tweets" ],
+		axisLabelFontSize: 10,
+		labelsDivWidth: 120,
+		labelsDivStyles: {
+	        'background-color': 'transparent',
+	        'font-size': '10px',
+			'text-align': 'right'
+		},
+	}
+);
 
+function unzoomGraph() {
+	g.updateOptions({
+		dateWindow: null,
+		valueRange: null
+	});
+}
 
 
 
