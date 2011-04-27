2521f74be9468602a2edea2eab3b41bdd046b69d
Chris Chua <chris.sirhc@gmail.com>
2011-04-26 21:53:50 -0400

    Working details view - Listens to "change:active" event to display   tweet details. - Does not remove tweets.

--- js/app/details.js
+++ js/app/details.js
@@ -1,9 +1,22 @@
 $(function(){
+	/*
+	* TweetDetail View
+	*/
+	window.TweetDetail = Backbone.View.extend({
+		tagName: "li",
+		template: _.template('<img src="<%= data.user.profile_image_url %>"/><p><span class="username"><%= data.user.name %></span> <%= data.text %></p>'),
+		initialize: function () {
+			this.model.bind('change:selected', this.render);
+		},
+		render: function () {
+			$(this.el).html(this.template(this.model.toJSON()));
+			return this;
+		}
+	});
 	
 	/*	DetailsView
 		
 	*/
-	
 	window.DetailsView = Backbone.View.extend({
 		
 		el: $("#details-container"),
@@ -13,7 +26,20 @@ $(function(){
 		},
 		
 		initialize: function(){
-			
+      _.bindAll(this, "contentChange");
+		  this.model.bind("change:active", this.contentChange);
+		},
+
+		contentChange: function (model) {
+			if(model.get('active') === true) {
+				model.get('tweets').each(function (tweet) {
+					console.log(tweet);
+					var tweetView = new TweetDetail({model: tweet});
+					this.$("#tweets-details").append(tweetView.render().el);
+				});
+			} else {
+				// Remove the nodes
+			}
 		},
 		
 		changeInterval: function(){
